8324df80b5fd9e9fe07e09f7c1a0558956ea863d
Pascalgri <pascal.grimm@outlook.de>
2026-04-17 11:30:06 +0200

    feat: continue sentence in chapter 3

--- main.cpp
+++ main.cpp
@@ -42,7 +42,7 @@ int main()
     std::cout << "Die Zwerge schmiedeten in der Hitze des Vulkans eine unzerstoerbare Klinge für das Maedchen." << std::endl;
 
     std::cout
-         << "Rotkaeppchen ging in den Wald hinein."
+         << "Rotkaeppchen ging in den Wald hinein ."
          << std::endl
          << std::endl
 
@@ -57,7 +57,7 @@ int main()
               << std::endl
               << std::endl; // erster Satz, neue Sätze in diesem Kapitel erst
                             // unter diesem Satz
-    std::cout << "Der Koenig war verzweifelt auf der Suche nach einer Loesung."
+    std::cout << "Der Koenig war verzweifelt auf der Suche nach einer Loesung und wendete sich an den Dorfältesten."
               << std::endl;
     /*
      * Viertes Kapitel
